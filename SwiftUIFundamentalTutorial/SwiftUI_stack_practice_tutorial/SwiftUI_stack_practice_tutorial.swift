dc5ba32d7baff090c227060866b3f397ab6d9b44
jieun <0330jieun@gmail.com>
2023-09-11 00:58:02 +0900

    Terminal Terminal [SwiftUI fundamental Tutorial] #8 Layout practice with stacks

---
+++ SwiftUIFundamentalTutorial/SwiftUI_stack_practice_tutorial/SwiftUI_stack_practice_tutorial.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUI_stack_practice_tutorial.swift
+//  SwiftUIFundamentalTutorial
+//
+//  Created by 박지은 on 2023/09/11.
+//
+
+import SwiftUI
+
+struct SwiftUI_stack_practice_tutorial: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+//struct SwiftUI_stack_practice_tutorial_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SwiftUI_stack_practice_tutorial()
+//    }
+//}
